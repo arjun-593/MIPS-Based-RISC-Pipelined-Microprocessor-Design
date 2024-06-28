2f7ee5a9f3f3f6bc641c64b7c6c9827574594e7e
Deepesh Bansal <135322025+DeepeshBansal@users.noreply.github.com>
2024-06-28 17:41:30 +0530

    Update processor_tb.v

--- processor_tb.v
+++ processor_tb.v
@@ -1,5 +1,3 @@
-// processor_tb.v
-
 `timescale 1ns / 1ps
 
 module processor_tb;
@@ -11,7 +9,7 @@ module processor_tb;
     // Outputs
     wire zero;
 
-    // Internal signals for monitoring
+
     wire [31:0] instruction_code;
     wire [31:0] PC;
     wire [3:0] alu_control;
@@ -23,17 +21,14 @@ module processor_tb;
     wire [4:0] write_reg;
     wire [31:0] write_data;
 
-    // Instantiate the Processor
     PROCESSOR uut (
         .clock(clock),
         .reset(reset),
         .zero(zero)
     );
 
-    // Clock generation
-    always #5 clock = ~clock; // Toggle clock every 5 time units (100 MHz clock)
-
-    // Assign internal signals for monitoring
+    always #5 clock = ~clock; 
+    
     assign instruction_code = uut.IFU_module.Instruction_Code;
     assign PC = uut.IFU_module.PC;
     assign alu_control = uut.control_module.alu_control;
@@ -46,19 +41,15 @@ module processor_tb;
     assign write_data = uut.datapath_module.write_data;
 
     initial begin
-        // Initialize Inputs
         clock = 0;
         reset = 0;
 
-        // Apply reset
-        #10; // Delay 10 time units
-        reset = 1; // Assert reset
-        #10; // Delay to allow reset propagation
-        reset = 0; // Deassert reset
-
-        // Run the simulation to observe waveforms
-        #100; // Run simulation for sufficient time to observe the outputs
-        $stop; // Stop the simulation
+        #10; 
+        reset = 1;
+        #10; 
+        reset = 0; 
+        #100;
+        $stop; 
 
     end
 
