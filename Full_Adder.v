4d4a37f4f0f417fe5aeab578be22b94df171878e
Arjun S Nair <121332996+arjun-593@users.noreply.github.com>
2024-07-12 08:18:33 +0530

    Create Full_Adder.v

---
+++ Full_Adder.v
@@ -0,0 +1,21 @@
+`timescale 1ns/1ns
+
+// Full Adder
+module Full_Adder(a,b,cin,sum,cout);
+  input a,b,cin;
+  output sum,cout;
+  wire x,y,z;
+ 
+  Half_Adder h1(.a(a),.b(b),.s(x),.c(y));
+  Half_Adder h2(.a(x),.b(cin),.s(sum),.c(z));
+  or o1(cout,y,z);
+endmodule : Full_Adder
+
+// Half Adder           
+module Half_Adder(a,b,s,c); 
+  input a,b;
+  output s,c;
+ 
+  xor x1(s,a,b); // Gate level design of half adder
+  and a1(c,a,b);
+endmodule :Half_Adder
